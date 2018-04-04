fe15ecdcf27cf2ce53577805281ffc9815e37983
Sabyasachi Roy Choudhury <sabyasachi087@gmail.com>
2018-04-04 15:32:32 +0530

    EDA completed. Next : Check for feature engg and regression options

--- eda.py
+++ eda.py
@@ -3,24 +3,30 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+import pandas as pd
 import curator as crt
 from DataType import DataType
 
-data_list = crt.getData(gesture='bye')
+data_list = crt.getData(gesture='correct')
 data = data_list[1]
-X = np.array(data.cord_x.astype('float64').values).astype(int)
-Y = np.array(data.cord_y.astype('float64').values).astype(int)
-Z = np.array(data.cord_z.astype('float64').values).astype(int)
+X = np.array(data.tilt_x.astype('float64').values).astype(int)
+Y = np.array(data.tilt_y.astype('float64').values).astype(int)
+Z = np.array(data.tilt_z.astype('float64').values).astype(int)
 print(data)
 
 # Plotting graph
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-
-ax.plot(X, Y, Z, label='parametric curve')
-ax.legend()
-
+data_0 = data_list[0]
+acc_ts = data[["tilt_x","tilt_y","tilt_z"]].cumsum()
+acc_ts_0 = data_0[["tilt_x","tilt_y","tilt_z"]].cumsum()
+acc_ts.plot()
+acc_ts_0.plot()
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# 
+# ax.scatter(X, Y, Z)
+# ax.legend()
+# ax.set_xlabel('X Label')
+# ax.set_ylabel('Y Label')
+# ax.set_zlabel('Z Label')
+plt.legend()
 plt.show()
